349f4325d6bf83ac5dca672f679ef9f055db3100
Janara Tolonbaeva <janaratolonbaeva@gmail.com>
2021-01-29 10:51:33 +0600

    add the blog project

---
+++ src/container/Blog/Blog.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "../../components/Header/Header";
+import Post from "../../components/Post/Post";
+import AddPost from "../../components/AddPost/AddPost";
+
+const Blog = () => {
+	return (
+		<>
+			<Header/>
+			<Post/>
+			<AddPost/>
+		</>
+	);
+};
+
+export default Blog;
